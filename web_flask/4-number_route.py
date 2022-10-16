b4a8818bd430e37ddb62ef49987bdeffa91fbe8a
Nabil <nabil.noble01@gmail.com>
2022-10-16 20:07:27 +0000

    Edit

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,43 +1,42 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
-
+"""Starts Flask web app"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Returns a string for /"""
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a string for /hbnb"""
-    return 'HBNB'
+    """prints HBNB"""
+    return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def ctext(text):
-    """Returns a string for /c/<text>"""
-    text2 = text.replace('_', ' ')
-    return "C %s" % text2
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
 
 
-@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text):
-    """Returns a string for /python/<text> or /python"""
-    text2 = text.replace('_', ' ')
-    return "Python %s" % text2
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """prints Python is cool"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """Returns a string for /number/<n>"""
-    if type(n) == int:
-        return '%i is a number\n' % n
+def number_n(n):
+    """display n if integer"""
+    return "%i is a number" % n
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
