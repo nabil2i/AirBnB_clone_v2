a4cb2b618163d69661ddef58ad02c1f22e304ae1
Nabil <nabil.noble01@gmail.com>
2022-10-17 09:52:44 +0000

    Edit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Returns a string for /"""
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a string for /hbnb"""
+    """returns HBNB"""
     return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def ctext(text):
-    """Returns a string for /c/<text>"""
-    text2 = text.replace('_', ' ')
-    return "C %s" % text2
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text):
-    """Returns a string for /python/<text> or /python"""
-    text2 = text.replace('_', ' ')
-    return "Python %s" % text2
-
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,51 +1,47 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+"""
+starts a Flask web application
+"""
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Returns a string for /"""
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a string for /hbnb"""
+    """returns HBNB"""
     return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def ctext(text):
-    """Returns a string for /c/<text>"""
-    text2 = text.replace('_', ' ')
-    return "C %s" % text2
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text):
-    """Returns a string for /python/<text> or /python"""
-    text2 = text.replace('_', ' ')
-    return "Python %s" % text2
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """Returns a string for /number/<n>"""
-    if type(n) == int:
-        return '%i is a number\n' % n
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Returns a HTML page for /number_template/<n>"""
-    if type(n) == int:
-        return render_template('5-number.html', n=n)
-
+def numbersandtemplates(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
